96c15a7f7dd1f91785d2ec3c1c0bfdaa5f2842bb
november295536 <peter840606@gmail.com>
2021-04-29 10:15:37 +0800

    feature: personal profile

---
+++ components/Profile.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+v-card.profile
+  h2 Hi, I'm november295536
+  v-divider.divider
+  p.introduce
+    | 台灣人，現居台北
+    br
+    | 軟體工程師
+    br
+    | 很愛買書，看完的比率有超過20％
+    br
+    | 貓狗通吃
+  v-divider.divider
+  .personal-links
+    h2 Where to find me
+    a.link(v-for="linkInfo in links", :href="linkInfo.link")
+      img.link-img(:src="linkInfo.img")
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          link: 'https://github.com/november295536',
+          img: '/github.png',
+        },
+        {
+          link: 'https://leetcode.com/november295536/',
+          img: '/LeetCode.png',
+        },
+        {
+          link: 'https://www.linkedin.com/in/chenghao-lee-0683a2111/',
+          img: '/linkedin.png',
+        },
+        {
+          link: 'mailto:peter840606@gmail.com',
+          img: '/mail.png',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.profile
+  width: 100%
+  padding: 15px
+  font-size: 1.2em
+.divider
+  margin: 10px 0
+.link-img
+  width: 28px
+  height: 28px
+  margin-right: 5px
+.introduce
+  margin-bottom: 10px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,29 @@
 <template lang="pug">
-  div.container
-    div
-      Logo
-      h1.title Nepeta
-      v-btn(@click="buttonOnClick") test test
-      div.links
-        a(href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green") Documentation
-        a(href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey") GitHub
+  .container
+    .post-list
+    .widget-area
+      Profile
 </template>
 
 <script>
-export default {
-  methods: {
-    async buttonOnClick() {
-      const posts = await this.$repository.posts.getAll()
-      console.log(posts)
-    },
-  },
-}
+export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="sass" scoped>
+.container
+  display: flex
+  height: fit-content
+  margin-right: auto
+  margin-left: auto
+  @include screen-until($sm)
+    display: block
+.post-list
+  flex: 3
+  width: 100%
+  padding: 0 15px
+  // background: RED
+.widget-area
+  flex: 1
+  // width: 390px
+  padding: 0 15px
 </style>
