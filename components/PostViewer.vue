e2b26c4e0fbc2551f42799f2565c3061d211e6ea
november295536 <peter840606@gmail.com>
2021-05-20 21:42:54 +0800

    feature: update post viewer. Now will display publish state

--- components/PostViewer.vue
+++ components/PostViewer.vue
@@ -1,18 +1,14 @@
 <template lang="pug">
 v-card.post
   .date {{ time }}
+  .published(v-if="!published") 未發布
   h2.title
     nuxt-link(:to="postPage") {{ title }}
   markdown-it-vue(:content="content")
 </template>
 
 <script>
-// import MarkdownItVue from 'markdown-it-vue'
-
 export default {
-  // components: {
-  //   MarkdownItVue,
-  // },
   props: {
     title: {
       type: String,
@@ -28,6 +24,10 @@ export default {
     },
     slug: {
       type: String,
+      default: undefined,
+    },
+    published: {
+      type: Boolean,
       required: true,
     },
   },
@@ -49,19 +49,20 @@ export default {
 <style lang="sass" scoped>
 .post
   padding: 3%
-  margin-bottom: 3%
   ::v-deep .markdown-body
-    font-size: 1.7em
-    line-height: 1.8em
+    font-size: 1.4em
+    line-height: 1.6em
     overflow-x: auto
 .date
-  font-size: 1.5em
+  font-size: 1.3em
   font-weight: 600
   color: grey
+.published
+  color: red
 .title
   line-height: inherit
   ::v-deep a
-    font-size: 3em
+    font-size: 2.5em
     text-decoration: none
     color: black
     &:hover
@@ -70,22 +71,22 @@ export default {
 @include screen-until($md)
   .post
     ::v-deep .markdown-body
-      font-size: 1.4em
+      font-size: 1.3em
       line-height: 1.5em
   .date
-    font-size: 1.4em
+    font-size: 1.1em
     font-weight: 600
   .title
     ::v-deep a
-      font-size: 2em
+      font-size: 1.5em
 
 @include screen-until($sm)
   .post
     ::v-deep .markdown-body
       font-size: 1em
-      line-height: 1.4em
+      line-height: 1.6em
   .date
-    font-size: 1.2em
+    font-size: 1em
     font-weight: 600
   .title
     ::v-deep a
