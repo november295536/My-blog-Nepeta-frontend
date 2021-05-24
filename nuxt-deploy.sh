c2f13d9b0a9594df8e0274f3d9b783192ea7ad97
november295536 <peter840606@gmail.com>
2021-05-25 06:48:45 +0800

    bug: fix can't show remote progess bar error and fix compressed file too big issue

--- nuxt-deploy.sh
+++ nuxt-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar cf - . -P | pv -s $(du -sb . | awk '{print $1}') | gzip > blog.tar.gz
-scp blog.tar.gz blog:/tmp
-ssh blog '. ~/.zshrc; sh /Nepeta/deploy.sh'
-rm blog.tar.gz
+tar cf - . -P | pv -s $(du -sb . | awk '{print $1}') | gzip > ../blog.tar.gz
+scp ../blog.tar.gz blog:/tmp
+ssh blog -t '. ~/.zshrc; sh /Nepeta/deploy.sh'
+rm ../blog.tar.gz
