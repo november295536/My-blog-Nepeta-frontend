e810b5c1bd24cba2e57f7e1e8ac3826f6f1b222a
november295536 <peter840606@gmail.com>
2021-05-25 08:54:10 +0800

    create github action file for auto build and deploy

--- nuxt-deploy.sh
+++ nuxt-deploy.sh
@@ -2,5 +2,5 @@
 
 tar cf - . -P | pv -s $(du -sb . | awk '{print $1}') | gzip > ../blog.tar.gz
 scp ../blog.tar.gz blog:/tmp
-ssh blog -t '. ~/.zshrc; sh /Nepeta/deploy.sh'
+ssh blog -tt '. ~/.zshrc; sh /Nepeta/deploy.sh'
 rm ../blog.tar.gz
