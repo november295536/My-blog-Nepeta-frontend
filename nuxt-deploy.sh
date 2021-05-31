df8fc601fa970091a50174cd05ff932bc38b675e
november295536 <peter840606@gmail.com>
2021-05-31 15:12:44 +0800

    update production target

--- nuxt-deploy.sh
+++ nuxt-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tar cf - . -P | pv -s $(du -sb . | awk '{print $1}') | gzip > ../blog.tar.gz
-scp ../blog.tar.gz blog:/tmp
-ssh blog -tt '. ~/.zshrc; sh /Nepeta/deploy.sh'
+scp ../blog.tar.gz nov29:/tmp
+ssh nov29 -tt '. ~/.zshrc; sh /Nepeta/deploy.sh'
 rm ../blog.tar.gz
